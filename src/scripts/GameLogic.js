b85045463e1223f3e2f3e705fdbb3e32315cd38a
Krystian <krkolodziej9@gmail.com>
2020-11-08 12:13:48 +0100

    make logic of the game

--- src/scripts/GameLogic.js
+++ src/scripts/GameLogic.js
@@ -5,7 +5,7 @@ class GameLogic {
         this.circlePlName = circlePlName;
         this.xPlName = xPlName;
         this.whoStart = Math.floor(Math.random() * 2) + 1;
-        
+        this.cells = [];   
         this.nextTurn();
     }
 
@@ -35,23 +35,59 @@ class GameLogic {
         this.main.appendChild(div)
     } 
     nextTurn() {
+        let turns = 0;
         
         this.whichTurnMessage()
         this.fields.forEach(field => field.addEventListener("click", () => {
+            turns++;
+            const index = field.getAttribute("index")
             const message = document.querySelector(".turn-message")
             if (this.whoStart == 1) {
                 field.appendChild(this.drawCircle())
+                this.cells[index] = 'O';
+                this.checkWinnerMsg(turns, 'O')
                 this.whoStart++;
-                this.whichTurnMessage()  
             } else {
                 field.appendChild(this.drawX())
+                this.cells[index] = 'X';
+                this.checkWinnerMsg(turns, 'X')
                 this.whoStart--;
-                this.whichTurnMessage()
             }
+            this.whichTurnMessage()
             
             message.parentNode.removeChild(message);
         }))
-    }    
+    } 
+    checkWinnerAlg(sign) {
+        const cells = this.cells;
+        if (cells[0] == sign && cells[0] == cells[1] && cells[1] == cells[2]){
+            return `${sign} wins`
+        } else if (cells[3] == sign && cells[3] == cells[4] && cells[4] == cells[5]){
+            return `${sign} wins`
+        } else if (cells[6] == sign && cells[6] == cells[7] && cells[7] == cells[8]){
+            return `${sign} wins`
+        } else if (cells[0] == sign && cells[0] == cells[3] && cells[3] == cells[6]){
+            return `${sign} wins`
+        } else if (cells[1] == sign && cells[1] == cells[4] && cells[4] == cells[7]){
+            return `${sign} wins`
+        } else if (cells[2] == sign && cells[2] == cells[5] && cells[5] == cells[8]){
+            return `${sign} wins`
+        } else if (cells[0] == sign && cells[0] == cells[4] && cells[4] == cells[8]){
+            return `${sign} wins`
+        } else if (cells[2] == sign && cells[2] == cells[4] && cells[4] == cells[6]){
+            return `${sign} wins`
+        }
+       
+    }
+    checkWinnerMsg(turns, sign) {
+        if (turns >= 5) {
+            switch(this.checkWinnerAlg(sign)) {
+                case `${sign} wins` :
+                    console.log(`${sign} wins`)
+                    break;
+            }
+        }
+    }   
    
 
 }
