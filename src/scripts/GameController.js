270b804e0ae4f82949e2df59d3be8ad8784074d4
Krystian <krkolodziej9@gmail.com>
2020-11-08 20:46:45 +0100

    almost perfect

--- src/scripts/GameController.js
+++ src/scripts/GameController.js
@@ -1,7 +1,7 @@
 import GameLogic from "./GameLogic.js";
 
-class GameController{
-    
+class GameController {
+
     constructor() {
         this.form = document.querySelector(".main__form");
         this.area = document.querySelector(".main__area");
@@ -11,31 +11,46 @@ class GameController{
         this.gameInit()
 
     }
-
+    checkCharsInNames(name) {
+        const arr = name.split("")
+        if (arr.length > 15) return false
+        return true;
+    }
     assignNames() {
         const inputCircle = document.querySelector(".form__input--circle")
         const inputX = document.querySelector(".form__input--x")
-
-        if (inputCircle && inputX) {
+        if (this.checkCharsInNames(inputCircle.value)) {
             this.circlePlName = inputCircle.value;
-            this.xPlName = inputX.value;
-            this.clear()
+        } else {
+            alert("Nieodpowiednia ilość znaków w graczu kółko");
+            return
         }
+        if (this.checkCharsInNames(inputX.value)) {
+            this.xPlName = inputX.value;
+        } else {
+            alert("Nieodpowiednia ilość znaków w graczu krzyżyk");
+            return
+        }
+        this.clear()
     }
     clear() {
         this.form.style.display = "none";
         this.fields.forEach(field => field.textContent = "")
     }
 
-  
+
     gameInit() {
-        this.form.addEventListener("submit",(e) => {
+        this.form.addEventListener("submit", (e) => {
             e.preventDefault();
             this.assignNames()
-            this.logic = new GameLogic(this.circlePlName, this.xPlName);
+            if (this.circlePlName && this.xPlName) {
+                this.logic = new GameLogic(this.circlePlName, this.xPlName);
+                this.logic.game()
+            }
+            
         })
     }
 
 }
 
-document.addEventListener("DOMContentLoaded", () =>  new GameController())
+document.addEventListener("DOMContentLoaded", () => new GameController())
--- src/scripts/GameLogic.js
+++ src/scripts/GameLogic.js
@@ -4,9 +4,9 @@ class GameLogic {
         this.main = document.querySelector(".main")
         this.circlePlName = circlePlName;
         this.xPlName = xPlName;
-        this.whoStart = Math.floor(Math.random() * 2) + 1;
-        this.cells = [];   
-        this.nextTurn();
+        this.whichTurn = Math.floor(Math.random() * 2) + 1;
+        this.popupBg;
+        this.cells = [];
     }
 
     drawCircle() {
@@ -25,7 +25,7 @@ class GameLogic {
     whichTurnMessage() {
         const div = document.createElement("div")
         div.classList.add("turn-message")
-        if (this.whoStart == 1) {
+        if (this.whichTurn == 1) {
             div.classList.add("circle-turn")
             div.innerHTML = `<p>Ruch gracza: <span>${this.circlePlName}</span></p>`
         } else {
@@ -33,62 +33,108 @@ class GameLogic {
             div.innerHTML = `<p>Ruch gracza: <span>${this.xPlName}</span></p>`
         }
         this.main.appendChild(div)
-    } 
-    nextTurn() {
+    }
+    game() {
         let turns = 0;
-        
         this.whichTurnMessage()
         this.fields.forEach(field => field.addEventListener("click", () => {
             turns++;
             const index = field.getAttribute("index")
             const message = document.querySelector(".turn-message")
-            if (this.whoStart == 1) {
-                field.appendChild(this.drawCircle())
-                this.cells[index] = 'O';
-                this.checkWinnerMsg(turns, 'O')
-                this.whoStart++;
-            } else {
-                field.appendChild(this.drawX())
-                this.cells[index] = 'X';
-                this.checkWinnerMsg(turns, 'X')
-                this.whoStart--;
+            if (!this.cells[index]) {
+                if (this.whichTurn == 1) {
+                    field.appendChild(this.drawCircle())
+                    this.cells[index] = 'O';
+                    this.checkWinnerMsg(turns, 'O')
+                    this.whichTurn++;
+                } else {
+                    field.appendChild(this.drawX())
+                    this.cells[index] = 'X';
+                    this.checkWinnerMsg(turns, 'X')
+                    this.whichTurn--;
+                }
+                this.whichTurnMessage()
+                message.parentNode.removeChild(message);
+
             }
-            this.whichTurnMessage()
-            
-            message.parentNode.removeChild(message);
         }))
-    } 
-    checkWinnerAlg(sign) {
-        const cells = this.cells;
-        if (cells[0] == sign && cells[0] == cells[1] && cells[1] == cells[2]){
-            return `${sign} wins`
-        } else if (cells[3] == sign && cells[3] == cells[4] && cells[4] == cells[5]){
-            return `${sign} wins`
-        } else if (cells[6] == sign && cells[6] == cells[7] && cells[7] == cells[8]){
-            return `${sign} wins`
-        } else if (cells[0] == sign && cells[0] == cells[3] && cells[3] == cells[6]){
-            return `${sign} wins`
-        } else if (cells[1] == sign && cells[1] == cells[4] && cells[4] == cells[7]){
-            return `${sign} wins`
-        } else if (cells[2] == sign && cells[2] == cells[5] && cells[5] == cells[8]){
-            return `${sign} wins`
-        } else if (cells[0] == sign && cells[0] == cells[4] && cells[4] == cells[8]){
-            return `${sign} wins`
-        } else if (cells[2] == sign && cells[2] == cells[4] && cells[4] == cells[6]){
-            return `${sign} wins`
-        }
-       
     }
-    checkWinnerMsg(turns, sign) {
+    checkWinnerAlg() {
+        const cells = this.cells;
+        if (cells[0] == 'O' && cells[0] == cells[1] && cells[1] == cells[2] ||
+            cells[3] == 'O' && cells[3] == cells[4] && cells[4] == cells[5] ||
+            cells[6] == 'O' && cells[6] == cells[7] && cells[7] == cells[8] ||
+            cells[0] == 'O' && cells[0] == cells[3] && cells[3] == cells[6] ||
+            cells[1] == 'O' && cells[1] == cells[4] && cells[4] == cells[7] ||
+            cells[2] == 'O' && cells[2] == cells[5] && cells[5] == cells[8] ||
+            cells[0] == 'O' && cells[0] == cells[4] && cells[4] == cells[8] ||
+            cells[2] == 'O' && cells[2] == cells[4] && cells[4] == cells[6]) {
+            return 'O wins';
+        } else if (cells[0] == 'O' && cells[0] == cells[1] && cells[1] == cells[2] ||
+            cells[3] == 'X' && cells[3] == cells[4] && cells[4] == cells[5] ||
+            cells[6] == 'X' && cells[6] == cells[7] && cells[7] == cells[8] ||
+            cells[0] == 'X' && cells[0] == cells[3] && cells[3] == cells[6] ||
+            cells[1] == 'X' && cells[1] == cells[4] && cells[4] == cells[7] ||
+            cells[2] == 'X' && cells[2] == cells[5] && cells[5] == cells[8] ||
+            cells[0] == 'X' && cells[0] == cells[4] && cells[4] == cells[8] ||
+            cells[2] == 'X' && cells[2] == cells[4] && cells[4] == cells[6]) {
+            return 'X wins';
+        } 
+    }
+    checkWinnerMsg(turns) {
         if (turns >= 5) {
-            switch(this.checkWinnerAlg(sign)) {
-                case `${sign} wins` :
-                    console.log(`${sign} wins`)
-                    break;
+            if (this.checkWinnerAlg()) {
+                this.popupBg = document.createElement("div")
+                this.popupBg.className = "popup-bg"
+                document.body.appendChild(this.popupBg)
+
+                const popup = document.createElement("div")
+                popup.className = "popup"
+                let winnerName;
+                if (this.checkWinnerAlg() === 'O wins') {
+                    winnerName = this.circlePlName;
+                } else if (this.checkWinnerAlg() === 'X wins') {
+                    winnerName = this.xPlName
+                } 
+                switch (winnerName) {
+                    case this.circlePlName:
+                        popup.innerHTML = `<p>Zwyciężył gracz <span class='cir'>${this.circlePlName}</span> (kółko). <strong>Gratulacje!</strong></p>`
+                        break;
+                    case this.xPlName:
+                        popup.innerHTML = `<p>Zwyciężył gracz <span class='x'>${this.xPlName}</span> (krzyżyk). <strong>Gratulacje!</strong></p>`
+                        break;
+                }
+                const button = this.revangeButton()
+                const img = document.createElement("img")
+                img.setAttribute("src", "src/images/medal.png")
+                this.popupBg.appendChild(popup)
+                popup.appendChild(img)
+                popup.appendChild(button)
+                this.main.style.filter = "blur(50px)"
             }
         }
-    }   
-   
+    }
+    revangeButton() {
+        const button = document.createElement("button")
+        button.className = "play-again"
+        button.textContent = "rewanż"
+        button.addEventListener("click", () => {
+            this.reset()
+        })
+        return button
+    }
+    
+    reset() {
+            this.popupBg.style.display = "none";
+            this.whichTurn = Math.floor(Math.random() * 2) + 1;
+            this.cells = []
+            this.fields.forEach(field => field.textContent = "")
+            this.main.style.filter = "blur(0)"
+            const message = document.querySelector(".turn-message")
+            message.parentNode.removeChild(message);
+            this.game()
+    }
+    
 
 }
 export default GameLogic;
